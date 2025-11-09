b17dafe1202c5a4b672041baa2915249b7586060
Robin.Wang <3Jg$94sbQ@q1>
2025-11-09 16:28:53 +0800

    feat(project): add tailwind and sass

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="test">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.test {
+  margin-left: 2rem;
+}
+</style>
