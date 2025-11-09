45719e47ea107f55bce2a0fe9f340d812dd22130
Robin.Wang <3Jg$94sbQ@q1>
2025-11-09 18:20:58 +0800

    feat(vite): add server config

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+    host: '0.0.0.0',
+    proxy: {
+      '/basic-api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^/basic-api`), ''),
+      },
     },
   },
 })
